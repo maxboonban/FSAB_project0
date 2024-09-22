f514ec0aa169910b2e3ca90b776bc85a8d95f583
maxboonban <maxboonban@gmail.com>
2024-09-21 23:11:15 -0400

    change back-end port to 3001

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,10 @@
 import express from 'express';
 
 const app = express();
-const port = 3000;
+const port = 3001;
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.send('Hello World (from Express back-end)!');
 });
 
 app.listen(port, () => {
